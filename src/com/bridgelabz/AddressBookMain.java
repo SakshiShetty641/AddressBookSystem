aad84b284e5bc52f9967ba57f8510c885945b2f3
Sakshi <sakshishetty1231997@gmail.com>
2021-08-12 22:38:17 +0530

    Added: To add a new contact to address book

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,100 +1,53 @@
 package com.bridgelabz;
+import java.util.ArrayList;
+import java.util.Scanner;
 /**
  * Purpose - To create an Address Book System
  * @author Sakshi Shetty
  * @version 16.0
  * @since 2021-08-12
  */
-class Contact {
-
-    private String firstName, lastName, address, city, state, email;
-    private int zip;
-    private long phoneNumber;
-
-    public Contact(String firstName, String lastName, String address, String city, String state, String email, int zip, long phoneNumber ){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.email = email;
-        this.zip = zip;
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getZip() {
-        return zip;
-    }
-
-    public void setZip(int zip) {
-        this.zip = zip;
-    }
-
-    public long getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(long phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-}
 
 public class AddressBookMain {
+    ArrayList<Contacts> arrayDetails = new ArrayList<Contacts>();;
+    Scanner sc = new Scanner(System.in);
+
+    /**
+     * This method is used to add details to address book
+     */
+    public void addDetails() {
+        Contacts info = new Contacts();
+        System.out.println("Enter the first name");
+        info.setFirstName(sc.nextLine());
+        System.out.println("Enter the last name");
+        info.setLastName(sc.nextLine());
+        System.out.println("Enter the address");
+        info.setAddress(sc.nextLine());
+        System.out.println("Enter the city");
+        info.setCity(sc.nextLine());
+        System.out.println("Enter the state");
+        info.setState(sc.nextLine());
+        System.out.println("Enter the email");
+        info.setEmail(sc.nextLine());
+        System.out.println("Enter the zip code");
+        info.setZip(sc.nextInt());
+        System.out.println("Enter the phone number");
+        info.setPhoneNumber(sc.nextLong());
+        arrayDetails.add(info);
+        sc.close();
+    }
+
+    /**
+     * This method is used to display the added information
+     */
+    public void display() {
+        System.out.println(arrayDetails);
+    }
+
     public static void main(String[] args) {
-        Contact details = new Contact("Sakshi", "Shetty", "Shantidham", "Mumbai", "Maharashtra", "abc@gmail.com", 400095, 1234567891);
         System.out.println("Welcome to Address Book Program");
-        System.out.println("You have created details as: FirstName-" + details.getFirstName() + " , LastName-"
-                + details.getLastName() + " , Address-" + details.getAddress() + " , City-" + details.getCity()
-                + ", State-" + details.getState() + " , Email-" + details.getEmail() + " , Zip-" + details.getZip()
-                + " , PhoneNumber-" + details.getPhoneNumber());
+        AddressBookMain details = new AddressBookMain();
+        details.addDetails();
+        details.display();
     }
 }
